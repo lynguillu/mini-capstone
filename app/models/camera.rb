0f78a093d9ed62f57dfd7bb8aa0a713511e5cbad
Lynda Guillu <lynda_guillu@att.net>
2017-03-26 21:02:20 -0500

    add sale item

--- app/models/camera.rb
+++ app/models/camera.rb
@@ -3,6 +3,12 @@ class Camera < ApplicationRecord
   def friendly_created_at
     created_at.strftime("%B %e,%Y")
   end
+
+  def sale_message
+    if price.to_f < 350
+      return "Sale Item"
+    else
+      return "Everyday Value"
+    end
+  end
 end
-
-
